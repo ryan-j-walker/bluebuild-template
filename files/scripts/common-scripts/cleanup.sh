347679cfa79516570e52f548301fb2cd1cf4d485
Ryan Walker <ryanwalker8484@gmail.com>
2025-06-23 11:58:58 -0400

    initial commit

--- files/scripts/common-scripts/cleanup.sh
+++ files/scripts/common-scripts/cleanup.sh
@@ -2,6 +2,7 @@
 
 set -oue pipefail
 
+echo "Cleaning up..."
 sed -i 's/AutomaticUpdatePolicy=.*/AutomaticUpdatePolicy=none/g' /etc/rpm-ostreed.conf
 curl -s https://raw.githubusercontent.com/ublue-os/bluefin/refs/heads/main/system_files/shared/usr/bin/podman-host -o /usr/bin/podman-host
 chmod +x /usr/bin/podman-host
